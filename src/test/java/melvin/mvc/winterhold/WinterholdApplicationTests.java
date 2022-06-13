75160e2e419f2944107db23ea4b6fd3dbd0986f5
melvin.brilliant <brilliantmelvin@gmail.com>
2022-06-13 11:23:47 +0700

    Initial commit

---
+++ src/test/java/melvin/mvc/winterhold/WinterholdApplicationTests.java
@@ -0,0 +1,13 @@
+package melvin.mvc.winterhold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterholdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
